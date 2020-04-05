6369907d82d9b45ac38e15831351ffccea82fcb1
alexcewing <61062645+alexcewing@users.noreply.github.com>
2020-04-05 18:52:44 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are a couple of functions that 1. take a matrix and 2. calculate and cache the inverse of the matrix
+## If the inverse has already been calculated, the second function gets the "cached" inverse and returns that instead of calculating it
 
-## Write a short comment describing this function
+
+## This creates a list where the list contains functions and a couple of objects that can hold the matrix and its inverse
+## for each of the functions, the environment is the list and they are able to access the objects that hold the matrix and its inverse
+## because that's the environment they were created in.
+## you can set the value of the matrix (x) in calling makeCacheMatrix or by using the set function within
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) invmat <<- solve
+  getinv <- function() invmat
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This takes as an argument a list created via makeCacheMatrix, uses the makeCacheMatrix$getinv function to test whether the inverse of the matrix
+## has been calculated or not. If it has (i.e. getinv doesn't return NULL), it returns the already saved inverse.
+## Otherwise, it calculates the inverse and uses the setinv function to store/cache it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invmat <- x$getinv()
+  if(!is.null(invmat)) {
+    message("getting cached data")
+    return(invmat)
+  }
+  data <- x$get()
+  invmat <- solve(data, ...)
+  x$setinv(invmat)
+  invmat
 }
